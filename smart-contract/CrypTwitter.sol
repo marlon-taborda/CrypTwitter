be41763179bb3e8ce7e763b74f984b9d9992eac5
mtaborda <mtaborda@bonadmin.com>
2023-08-11 18:22:58 -0300

    New features: profile picture, list tweets by author

--- smart-contract/CrypTwitter.sol
+++ smart-contract/CrypTwitter.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.18;
 
+//TODO dynamic pagination defined by contract administrator
+
 struct Tweet {
     address author;
     string text;
@@ -19,8 +21,18 @@ contract CrypTwitter{
 
     mapping(address => string) public usernames;
 
+    mapping(address => string) public userProfileImageURLs;
+
+    constructor(){
+        address admin = msg.sender;
+    }
+
     function setUsername(string calldata name) public {
         usernames[msg.sender] = name;
+    }
+
+    function setUserProfileImageURL(string calldata url) public {
+        userProfileImageURLs[msg.sender] = url;
     }
 
     function addTweet(string calldata text) public {
@@ -40,8 +52,21 @@ contract CrypTwitter{
         for (uint i = 0; i < PAGE_SIZE; i++) {
             pageTweets[i] = tweets[startIndex + i];
             pageTweets[i].username = usernames[pageTweets[i].author];
-        }
+        }            
         return pageTweets;
+    }
+
+    //returning error: The called function should be payable if you send value and the value you send should be less than your current balance.
+    function findTweetsByAuthor(address author) public view returns(Tweet[] memory) {
+        Tweet[] memory foundTweets;
+        uint indexFound;
+        for (uint i = 0; i < tweetsIndex; i++) {
+            if(tweets[i].author == author){
+                indexFound++;
+                foundTweets[indexFound]=tweets[i];
+            }
+        }
+        return foundTweets;
     }
 
     function eraseTweet(uint tweetIndex) public {
